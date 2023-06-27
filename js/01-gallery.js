518fe7083e4a10e3e75847cd65357feb0f7d6717
Iryna Didkivska <ididkivska@gmail.com>
2023-06-27 18:58:33 +0200

    create gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+ 
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+galleryContainer.addEventListener('click',onGalleryContainerClick)
+    // Створення і рендер розмітки
+function createGalleryItemsMarkup(galleryItems) { 
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a href="${original}" class="gallery__link">
+            <img
+            loading="lazy"
+            src="${preview}" 
+            alt="${description}" 
+            data-source="${original}"
+            class="gallery__image">
+            </a>
+            </li>`
+        }
+        )
+        .join('');
+}
+// Реалізація делегування
+function onGalleryContainerClick(event) { 
+    event.preventDefault();
+    const { target: { classList, dataset } } = event;
+    const isGalleryImageEl = classList.contains('gallery__image');
+     if (!isGalleryImageEl) {
+    return;
+     }
+    //   Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
+    const bannerImgEl = basicLightbox.create(`
+    <img src="${dataset.source}">`);
+    bannerImgEl.show()
+    document.addEventListener('keydown', closeModalWindow);
+    // закриття модального вікна після натискання клавіші Escape
+    function closeModalWindow(event) {
+    if (event.key === 'Escape') {
+        bannerImgEl.close();
+    }
+}
+}
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) { 
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a href="${original}" class="gallery__link">
+            <img
+            loading="lazy"
+            src="${preview}" 
+            alt="${description}" 
+            data-source="${description}"
+            class="gallery__image">
+            </a>
+            </li>`
+        }
+        )
+        .join('');
+}
+a
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+
+
+const lightbox = new SimpleLightbox('.gallery a',
+    { /* options */
+        navText: ['&#10094', '&#10095'],
+        closeText: '&#10007',
+  
+    });
+
+console.log(galleryItems);
