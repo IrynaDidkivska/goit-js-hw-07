c00f0f5433a0ec36b081140817e741de38fb4e7d
Iryna Didkivska <ididkivska@gmail.com>
2023-06-29 23:41:31 +0200

    add captionsData

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -28,6 +28,10 @@ const lightbox = new SimpleLightbox('.gallery a',
     { /* options */
         navText: ['&#10094', '&#10095'],
         closeText: '&#10007',
+        captions: true,
+        captionsData:	'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
   
     });
 
